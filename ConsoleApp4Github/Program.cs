58cc82d2093df20e69ebc3fed9eb108862ca6832
Omar Saleh <Omar Saleh@ROWAD.LOCAL>
2020-07-01 03:51:47 +0200

    Add project files.

---
+++ ConsoleApp4Github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
